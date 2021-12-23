1082a18c5cedf901676f11325f0c72dfe6db5126
aysan <aysanbidariiii@gmail.com>
2021-12-23 15:11:05 +0330

    sumItems-added

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -6,6 +6,11 @@ const initialState ={
     total : 0,
     checkout : false
 }
+const sumItems = items => {
+    const itemsCounter = items.reduce((total, product) => total + product.quantity, 0);
+    let total = items.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2);
+    return {itemsCounter, total}
+}
 
 
 const cartReducer = (state , action) => {
@@ -20,25 +25,33 @@ const cartReducer = (state , action) => {
             } 
                 return {
                 ...state,
-                selectedItems:[...state.selectedItems]
+                selectedItems:[...state.selectedItems],
+                ...sumItems(state.selectedItems)
+
             }
             case "REMOVE_ITEM" :
                 const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id);
                 return {
                     ...state,
-                    selectedItems : [...newSelectedItems]
+                    selectedItems : [...newSelectedItems],
+                    ...sumItems(state.selectedItems)
+
                 }
             case "INCREASE":
                 const indexI = state.selectedItems.findIndex(item => item.id === action.payload.id);
                 state.selectedItems[indexI].quantity++;
                 return {
                        ...state,
+                       ...sumItems(state.selectedItems)
+
                    }
             case "DECREASE":
                 const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id);
                 state.selectedItems[indexD].quantity--;
                 return {
                         ...state,
+                        ...sumItems(state.selectedItems)
+
                     }
             case "CHECKOUT":
                 return{
